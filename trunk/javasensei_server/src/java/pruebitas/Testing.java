770ca0fbdcf91a9658afca08308d4c2485ba8c7e
roramas@gmail.com <roramas@gmail.com@d9df598c-e296-408c-98b3-72ee3ba90542>
2015-01-13 19:00:12 +0000

    Trabajando con CursoManager y los ejercicios.json.  Se borra el paquete dominio.algs ya que no se ocupaba.

--- trunk/javasensei_server/src/java/pruebitas/Testing.java
+++ trunk/javasensei_server/src/java/pruebitas/Testing.java
@@ -1,18 +1,9 @@
 package pruebitas;
 
-import com.google.gson.Gson;
-import java.util.ArrayList;
-import java.util.List;
+import javasensei.db.managments.CursoManager;
 
 public class Testing {
     public static void main(String[] args) {
-        List<Reactivo> reactivos = new ArrayList<>();
-        reactivos.add(new Reactivo("pregunta 1", 1));
-        reactivos.add(new Reactivo("pregunta 2", 1));
-        reactivos.add(new Reactivo("pregunta 3", 1));
-        
-        Gson gson = new Gson();
-        String strGson = gson.toJson(reactivos);
-        System.out.println(strGson);
+        CursoManager cm = new CursoManager();
     }
 }
