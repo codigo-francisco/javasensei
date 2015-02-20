1bd92cdd67dd7390d39c6903827e5b58078a35fb
Francisco Gonzalez Hernandez <franciscogonzalez_hernandez@hotmail.com>
2015-02-20 20:57:05 +0000

    Se versiona avances para implementar plugin de camara web

--- javasensei_server/web/js/camera.js
+++ javasensei_server/web/js/camera.js
@@ -1,18 +1,15 @@
 var camera_context = {};
 
-var camera = function (videoId, canvasId, ancho, largo) {
-    this.canvas_tag = document.getElementById(canvasId.slice(1));
-    this.video_tag = document.getElementById(videoId.slice(1));
-    this.ancho = ancho;
-    this.largo = largo;
-    this.context = {};
+var camera = function () {
+
     this.limite = 3; //Para modificar la cantidad de fotos a almacenar
     this.milisegundos = 1000; //1000 milisegundos es una foto
     this.id_interval = 0;
     this.url = "emocion/";
 
     this.tipoCamino = "";
-    this.funcionEmocion = function () {};
+    this.funcionEmocion = function () {
+    };
 
     this.photo_list = new buckets.LinkedList(); //Almacenamiento FIFO
 
@@ -22,6 +19,7 @@ var camera = function (videoId, canvasId, ancho, largo) {
 camera.prototype = {
     inicializarFotos: function () {
         camera_context.reiniciarFotos();
+
         //Se toma una foto de acuerdo a los milisegundos marcados
         camera_context.id_interval = setInterval(camera_context.snap, camera_context.milisegundos);
     },
@@ -29,48 +27,35 @@ camera.prototype = {
         camera_context.photo_list.clear();
     },
     detenerFotos: function () {
-        //Limpiamos la lista
+
         clearInterval(camera_context.id_interval);
-    },
-    setup: function () {
-
-        var videoObj = {audio:false, 
-            video: {mandatory: {minWidth: camera_context.ancho, minHeight: camera_context.largo}}};
-
-        navigator.userMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-        navigator.userMedia(videoObj, function (stream) { //Connection
-            camera_context.video_tag.src = window.URL.createObjectURL(stream);
-            camera_context.video_tag.play();
-
-            //Quitamos el fullscreen de la camara
-            eliminarBackgroundCamera();
-
-            camera_context.setupCanvas(); //Ajustamos el canvas a la camara
-        }, function (error) { //Error en la conexion
-            console.error(error);
-            //alert("Necesita habilitar la camara o usar un navegador con HTML 5");
-        });
-    },
-    setupCanvas: function () {
-        var canvas = camera_context.canvas_tag;
-
-        camera_context.context = canvas.getContext("2d");
-
-        canvas.width = camera_context.ancho;
-        canvas.height = camera_context.largo;
+        camera_context.reiniciarFotos();
     },
     snap: function () {
 
-        camera_context.context.drawImage(camera_context.video_tag, 0, 0, camera_context.ancho, camera_context.largo);
+        Webcam.snap(function (data) {
+            var image = camera_context.canvas_tag.toDataURL("image/jpeg").replace("data:image/jpeg;base64,", "");
 
-        var image = camera_context.canvas_tag.toDataURL("image/jpeg").replace("data:image/jpeg;base64,", "");
+            camera_context.photo_list.add(image);
 
-        camera_context.photo_list.add(image);
+            if (camera_context.photo_list.size() > camera_context.limite) {
+                camera_context.photo_list.removeElementAtIndex(0); //Primer item
+            }
+        });
+    },
+    setup: function () {
+        //Configuracion de la camara
+        Webcam.set({
+            dest_width: 800,
+            dest_height: 600,
+            image_format: 'jpeg',
+            jpeg_quality: 100,
+            force_flash: false
+        });
 
-        if (camera_context.photo_list.size() > camera_context.limite) {
-            camera_context.photo_list.removeElementAtIndex(0); //Primer item
-        }
+        Webcam.on("live", eliminarBackgroundCamera);
+
+        Webcam.attach('#camera');
     },
     evaluarEmociones: function (callback, datos) {
         var url_emocion = url + camera_context.url + "obteneremocion";
@@ -87,7 +72,7 @@ camera.prototype = {
             jsonpCallback: "jsonpCallback"
         }).done(function (datos) {
             //camera_context.funcionEmocion(camera_context.tipoCamino, emocion);
-            console.log("%c Emocion recibida:%O","color:white; background:black;",datos);
+            console.log("%c Emocion recibida:%O", "color:white; background:black;", datos);
             usuario.emocionPrevia = usuario.emocionActual;
             usuario.emocionActual = datos.emocion;
             camera_context.funcionEmocion(camera_context.tipoCamino);
@@ -95,7 +80,7 @@ camera.prototype = {
             console.error("Fallo: " + textStatus);
             console.error("Error: " + errorThrown);
         });
-        
+
         camera_context.reiniciarFotos();
     }
 };
