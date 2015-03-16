bf81e220196e45a3c8841f6ca186c9eff33460d7
Francisco Gonzalez Hernandez <franciscogonzalez_hernandez@hotmail.com>
2015-03-16 05:06:30 +0000

    Se versionan cambios en bd (para habilitar la autentificacion)

--- mongobd/functions.js
+++
@@ -1,8 +0,0 @@
-db.system.js.save(
-   {
-     _id: "ultimoIdAlumno",
-     value : function() { return db.alumnos.count(); }
-   }
-);
-
-db.loadServerScripts();
--- mongobd/run.bat
+++ mongobd/run.bat
@@ -1,3 +1,3 @@
 C:\
 cd C:\Program Files\MongoDB 2.6 Standard\bin
-mongod --dbpath "E:\Java Sensei\java-sensei\mongobd"
+mongod --dbpath "E:\Java Sensei\trunk\mongobd"
