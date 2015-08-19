16e4168793b055f0796bcdd2e2ea7971d624acd8
rockbass2560 <rockbass2560@d9df598c-e296-408c-98b3-72ee3ba90542>
2015-08-19 08:00:05 +0000

    Se versiona inicios de herramienta para creacion de grafos de dominio

---
+++ trunk/BuildGraph/plantillas.js
@@ -0,0 +1,27 @@
+//Plantilla inicial
+var plantillaInicial = {
+	id:"numero",
+	paso:"numero",
+	tipo:"enumeradorpaso",
+	instruccionesejercicio:"texto",
+	opciones:"arreglo"
+};
+
+var plantillaPasos={
+	"paso": "numero",
+	"tipo": "enumeradorpaso",
+	"instruccion": "texto",
+	"presentacion": "html",
+	"textoEmocional": {
+		"neutral": "texto",
+		"encantado": "texto",
+		"sorprendido": "texto",
+		"compasivo": "texto",
+		"esceptico": "texto",
+		"retroalimentacion": {
+			"positiva": "texto",
+			"neutral": "texto",
+			"negativa": "texto"
+		} 
+	}
+}
