dd03dc75c87285d5e9e188b83d071a0c13d3ee3b
rockbass2560 <franciscogonzalez_hernandez@hotmail.com>
2015-11-27 01:13:50 -0700

    Se versiona merge con cambios de yukie y pebble, ademas se ordena el menu  por id (tanto lecciones como ejercicios)

--- javasensei/src/main/java/javasensei/db/managments/MenuManager.java
+++ javasensei/src/main/java/javasensei/db/managments/MenuManager.java
@@ -11,6 +11,7 @@ import com.mongodb.DBObject;
 import com.mongodb.QueryBuilder;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -75,16 +76,21 @@ public class MenuManager {
     public String getCursoMenu(Long idAlumno) {
         BasicDBList list = new BasicDBList();
 
-        List<DBObject> ejercicios = Arrays.stream(((BasicDBList) alumnosCollections.findOne(
-                new BasicDBObject("id", idAlumno)
-        ).get("ejercicios")).toArray()).map((ejercicioObj) -> (DBObject) ejercicioObj).collect(Collectors.toList());
-
+        List<DBObject> ejercicios = Arrays.stream(
+            ((BasicDBList) alumnosCollections.findOne(
+                    new BasicDBObject("id", idAlumno)
+                ).get("ejercicios"))
+            .toArray())
+            .map((ejercicioObj) -> (DBObject) ejercicioObj)
+            .sorted((DBObject o1, DBObject o2) -> (int)o1.get("id")-(int)o2.get("id"))
+            .collect(Collectors.toList());
+        
         DBCursor cursor = leccionesCollection.find(QueryBuilder.start().get(),
                 QueryBuilder.start("nombre").is(1)
                 .put("id").is(1)
                 .put("_id").is(0)
                 .get()
-        );
+        ).sort(new BasicDBObject("id",1));
         
         while (cursor.hasNext()) {
             DBObject object = cursor.next();
@@ -101,11 +107,6 @@ public class MenuManager {
                 object.put("ejercicios", o);
                 list.add(object);
             }
-            
-            
-            
-            
-            
         }
 
         return list.toString();
