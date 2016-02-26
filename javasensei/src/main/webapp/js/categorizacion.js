d91c1e14bfc0b02e8bf1ff0fa3d0b9cc295eeb98
rockbass2560 <franciscogonzalez_hernandez@hotmail.com>
2016-02-26 13:49:33 -0700

    Se versiona avance, el carousel se hara manualmente

--- javasensei/src/main/webapp/js/categorizacion.js
+++ javasensei/src/main/webapp/js/categorizacion.js
@@ -1,38 +1,39 @@
+var ultimas_fotos=[];
 function iniciarCategorizacion(){    
     //Obtener fotografias
     $.post(url+"bitacora/obtenerbitacoracategorizacion",
         {idUsuario:usuario.id},
         function(result){
             var idElemento = "#paginacategorizacion";
-            var trackFotos = $("#trackFotos");
             
-            trackFotos.empty();
             //Por alguna razon jQuery no lo esta haciendo automaticamente
             result = JSON.parse(result);
+            ultimas_fotos = result;
             
             if (result.length>0){
-                //Construir html
-                for (var objeto in result){
-                    objeto = result[objeto];
-                    trackFotos.append(
-                        $("<li>")
-                        .addClass("glide__slide")
-                        .append(
-                            $("<img>").prop("src","data:image/jpeg;base64,"+objeto.fotografia)
-                        )
-                    );
-                }
+                crearCarousel(result);
             }
-            
-            $("#fotografiascategorizacion").glide({
-                type: "carousel",
-                autoplay:false,
-                centered:true,
-                autoheight:true
-            });
             
             //Inicializar la categorización por parte del usuario
             $(":mobile-pagecontainer").pagecontainer("change", idElemento);    
         }
     );
+}
+
+function crearCarousel(datos){
+    var trackFotos = $("#trackFotos");
+    trackFotos.empty();
+    
+    for (var objeto in datos){
+        objeto = datos[objeto];
+        trackFotos.append(
+            $("<li>")
+            .append(
+                $("<img>")
+                .prop("src","data:image/jpeg;base64,"+objeto.fotografia)
+            )
+        );
+    }
+    
+    trackFotos.itemslide();
 }
