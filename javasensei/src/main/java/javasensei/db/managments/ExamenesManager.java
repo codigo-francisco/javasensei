a1d8af43fce1c6fc3745cb54be5d20dbec453fd1
roramas <roramas@gmail.com>
2017-01-27 17:37:46 -0700

    Cambios para realizar los cuestionarios con los nuevos 'requerimientos' de la Dra. Barron

--- javasensei/src/main/java/javasensei/db/managments/ExamenesManager.java
+++ javasensei/src/main/java/javasensei/db/managments/ExamenesManager.java
@@ -7,6 +7,8 @@ import com.mongodb.DBObject;
 import com.mongodb.util.JSON;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 import javasensei.db.Connection;
 import javasensei.util.FileHelper;
 
@@ -15,47 +17,58 @@ import javasensei.util.FileHelper;
  * @author oramas
  */
 public class ExamenesManager {
-
-    private static final DBCollection pretestCollection = Connection.getCollection().get(CollectionsDB.PRETEST);
-    private static final DBCollection posttestCollection = Connection.getCollection().get(CollectionsDB.POSTTEST);
     
+    public enum TipoExamen{
+        PRETEST,
+        POSTTEST
+    }
+
+    public static final DBCollection pretestCollection = Connection.getCollection().get(CollectionsDB.PRETEST);
+    public static final DBCollection posttestCollection = Connection.getCollection().get(CollectionsDB.POSTTEST);
+    
+    private static final Map<String,DBObject> examenesPreTest = new HashMap<>();
+    private static final Map<String,DBObject> examenesPostTest = new HashMap<>();
     //Respuestas de los cuestionarios
-    static final DBObject jsonRespuestasPreTestA = (DBObject) JSON.parse(FileHelper.getInstance()
-                .getContentFile("examenes/respuestas_pretest.json"));
-    
-    static final DBObject jsonRespuestasPreTestB = (DBObject) JSON.parse(FileHelper.getInstance()
-                .getContentFile("examenes/respuestas_pretestB.json"));
-    
-    static final DBObject jsonRespuestasPreTestC = (DBObject) JSON.parse(FileHelper.getInstance()
-                .getContentFile("examenes/respuestas_pretestC.json"));
-    
-    static final DBObject jsonRespuestasPostTestA = (DBObject) JSON.parse(FileHelper.getInstance()
-                .getContentFile("examenes/respuestas_posttest.json"));
-    
-    static final DBObject jsonRespuestasPostTestB = (DBObject) JSON.parse(FileHelper.getInstance()
-                .getContentFile("examenes/respuestas_posttestB.json"));
-    
-    static final DBObject jsonRespuestasPostTestC = (DBObject) JSON.parse(FileHelper.getInstance()
-                .getContentFile("examenes/respuestas_posttestC.json"));
-    
-    
-    public String calificarExamenPreTest(String idFacebook, String jsonRespuestas, String tipoExamen) {
-
-        DBObject jsonRespuestasPreTest = new BasicDBObject();
+    static{
+        //Pretest
+        examenesPreTest.put("A", (DBObject) JSON.parse(FileHelper.getInstance()
+                .getContentFile("examenes/respuestas_pretest.json")));
         
-        switch (tipoExamen) {
-            case "A":
-                jsonRespuestasPreTest = jsonRespuestasPreTestA;
+        examenesPreTest.put("B", (DBObject) JSON.parse(FileHelper.getInstance()
+                .getContentFile("examenes/respuestas_pretestB.json")));
+        
+        examenesPreTest.put("C", (DBObject) (DBObject) JSON.parse(FileHelper.getInstance()
+                .getContentFile("examenes/respuestas_posttest.json")));
+        
+        //Posttest
+        examenesPostTest.put("A", (DBObject) JSON.parse(FileHelper.getInstance()
+                .getContentFile("examenes/respuestas_posttest.json")));
+        examenesPostTest.put("B", (DBObject) JSON.parse(FileHelper.getInstance()
+                .getContentFile("examenes/respuestas_posttestB.json")));
+        examenesPostTest.put("C", (DBObject) JSON.parse(FileHelper.getInstance()
+                .getContentFile("examenes/respuestas_posttest.json")));        
+    }
+    
+    public String calificarExamen(String idFacebook, String jsonRespuestas, String tipoCuestionario, TipoExamen tipoExamen) {
+
+        Map<String, DBObject> coleccionRespuestas = null;
+                
+        switch(tipoExamen){
+            case PRETEST:
+                coleccionRespuestas = examenesPreTest;
                 break;
-            case "B":
-                jsonRespuestasPreTest = jsonRespuestasPreTestB;
-                break;
-            case "C":
-                jsonRespuestasPreTest = jsonRespuestasPreTestC;
+            case POSTTEST:
+                coleccionRespuestas = examenesPostTest;
                 break;
         }
+        
+        DBObject jsonRespuestasBuenas = coleccionRespuestas.get(tipoCuestionario);
 
-        double cantidadPreguntas = jsonRespuestasPreTest.keySet().size();
+        DBObject respuestasBuenasDiagnostico = (DBObject)jsonRespuestasBuenas.get("diagnostico");
+        double cantidadPreguntasDiagnostico = respuestasBuenasDiagnostico.keySet().size();
+        DBObject respuestasBuenasPrueba = (DBObject)jsonRespuestasBuenas.get("prueba");
+        double cantidadPreguntasPrueba = (respuestasBuenasPrueba).keySet().size();
+        double cantidadTotalPreguntas = cantidadPreguntasDiagnostico+cantidadPreguntasPrueba;
 
         BasicDBList examen = (BasicDBList) com.mongodb.util.JSON.parse(jsonRespuestas);
 
@@ -63,101 +76,55 @@ public class ExamenesManager {
 
         if (examen.size() > 0) {
 
-            double totalRespuestasCorrectas = 0;
+            double totalRespuestasCorrectasDiagnostico = 0;
+            double totalRespuestasCorrectasPrueba = 0;
 
             for (Object jsonRespuesta : examen.toArray()) {
                 DBObject json = (DBObject) jsonRespuesta;
-
+                
                 String pregunta = json.get("pregunta").toString();
                 String respuesta = json.get("respuesta").toString();
 
-                if (jsonRespuestasPreTest.get(pregunta).equals(respuesta)) {
-                    totalRespuestasCorrectas++;
+                if (pregunta.startsWith("d")){
+                    if (respuestasBuenasDiagnostico.get(pregunta).equals(respuesta)){
+                        totalRespuestasCorrectasDiagnostico++;
+                    }
+                }else if (pregunta.startsWith("q")){
+                    if (respuestasBuenasPrueba.get(pregunta).equals(respuesta)){
+                        totalRespuestasCorrectasPrueba++;
+                    }
                 }
             }
 
-            double promedio = totalRespuestasCorrectas / cantidadPreguntas;
+            double promedioDiagnostico = totalRespuestasCorrectasDiagnostico / cantidadPreguntasDiagnostico;
+            double promedioPrueba = totalRespuestasCorrectasPrueba / cantidadPreguntasPrueba;
 
             insert.put("idFacebook", idFacebook);
             insert.put("examen", examen);
-            insert.put("tipoExamen", tipoExamen);
-            insert.put("totalRespuestasCorrectas", totalRespuestasCorrectas);
-            insert.put("promedio", promedio);
-            insert.put("cantidadPreguntas", cantidadPreguntas);
+            insert.put("tipoExamen", tipoCuestionario);
+            insert.put("totalRespuestasCorrectasDiagnostico", totalRespuestasCorrectasDiagnostico);
+            insert.put("promedioDiagnostico", promedioDiagnostico);
+            insert.put("cantidadPreguntasDiagnostico", cantidadPreguntasDiagnostico);
+            insert.put("totalRespuestasCorrectasPrueba", totalRespuestasCorrectasPrueba);
+            insert.put("promedioPrueba", promedioPrueba);
+            insert.put("cantidadPreguntasPrueba", cantidadPreguntasPrueba);
+            insert.put("cantidadTotalPreguntas", cantidadTotalPreguntas);
             LocalDateTime dateTime = LocalDateTime.now();
             insert.put("fecha", dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE));
             insert.put("hora", dateTime.format(DateTimeFormatter.ISO_LOCAL_TIME));
-
-            pretestCollection.save(insert);
-        }
-
-        return insert.toString();
-    }
-
-    public String realizoExamenPreTest(String idFacebook) {
-        DBObject result = pretestCollection.findOne(
-                new BasicDBObject("idFacebook", idFacebook)
-        );
-        return new BasicDBObject("realizado", result != null).toJson();
-    }
-    
-    public String calificarExamenPostTest(String idFacebook, String jsonRespuestas, String tipoExamen) {
-
-        DBObject jsonRespuestasPostTest = new BasicDBObject();
-        
-        switch (tipoExamen) {
-            case "A":
-                jsonRespuestasPostTest = jsonRespuestasPostTestA;
-                break;
-            case "B":
-                jsonRespuestasPostTest = jsonRespuestasPostTestB;
-                break;
-            case "C":
-                jsonRespuestasPostTest = jsonRespuestasPostTestC;
-                break;
-        }
-
-        double cantidadPreguntas = jsonRespuestasPostTest.keySet().size();
-
-        BasicDBList examen = (BasicDBList) com.mongodb.util.JSON.parse(jsonRespuestas);
-
-        DBObject insert = new BasicDBObject();
-
-        if (examen.size() > 0) {
-
-            double totalRespuestasCorrectas = 0;
-
-            for (Object jsonRespuesta : examen.toArray()) {
-                DBObject json = (DBObject) jsonRespuesta;
-
-                String pregunta = json.get("pregunta").toString();
-                String respuesta = json.get("respuesta").toString();
-
-                if (jsonRespuestasPostTest.get(pregunta).equals(respuesta)) {
-                    totalRespuestasCorrectas++;
-                }
+            
+            if(tipoExamen == TipoExamen.PRETEST){
+                pretestCollection.save(insert);
+            }else if (tipoExamen == TipoExamen.POSTTEST){
+                posttestCollection.save(insert);
             }
-
-            double promedio = totalRespuestasCorrectas / cantidadPreguntas;
-
-            insert.put("idFacebook", idFacebook);
-            insert.put("examen", examen);
-            insert.put("tipoExamen", tipoExamen);
-            insert.put("totalRespuestasCorrectas", totalRespuestasCorrectas);
-            insert.put("promedio", promedio);
-            insert.put("cantidadPreguntas", cantidadPreguntas);
-            LocalDateTime dateTime = LocalDateTime.now();
-            insert.put("fecha", dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE));
-            insert.put("hora", dateTime.format(DateTimeFormatter.ISO_LOCAL_TIME));
-
-            posttestCollection.save(insert);
         }
 
         return insert.toString();
     }
     
-    public String realizoExamenPostTest(String idFacebook) {
-        DBObject result = posttestCollection.findOne(
+    public String realizoExamen(String idFacebook, DBCollection examenCollection) {
+        DBObject result = examenCollection.findOne(
                 new BasicDBObject("idFacebook", idFacebook)
         );
         return new BasicDBObject("realizado", result != null).toJson();
--- javasensei/src/main/java/javasensei/rest/ExamenesRest.java
+++ javasensei/src/main/java/javasensei/rest/ExamenesRest.java
@@ -12,6 +12,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javasensei.db.managments.ExamenesManager.TipoExamen;
 
 /**
  * REST Web Service
@@ -38,17 +39,8 @@ public class ExamenesRest {
             @FormParam("json") String jsonRespuestas,
             @FormParam("tipoExamen") String tipoExamen){
         
-        return new ExamenesManager().calificarExamenPreTest(idFacebook, jsonRespuestas,tipoExamen);
-    }
-    
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("realizoExamenPreTest")
-    public String realizoExamenPreTest(
-            @QueryParam("idFacebook") String idFacebook){
-        return new ExamenesManager().realizoExamenPreTest(idFacebook);
-    }
-    
+        return new ExamenesManager().calificarExamen(idFacebook, jsonRespuestas,tipoExamen,TipoExamen.PRETEST);
+    }    
     
     @POST
     @Produces(MediaType.APPLICATION_JSON)
@@ -58,7 +50,15 @@ public class ExamenesRest {
             @FormParam("json") String jsonRespuestas,
             @FormParam("tipoExamen") String tipoExamen){
         
-        return new ExamenesManager().calificarExamenPostTest(idFacebook, jsonRespuestas,tipoExamen);
+        return new ExamenesManager().calificarExamen(idFacebook, jsonRespuestas,tipoExamen,TipoExamen.POSTTEST);
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("realizoExamenPreTest")
+    public String realizoExamenPreTest(
+            @QueryParam("idFacebook") String idFacebook){
+        return new ExamenesManager().realizoExamen(idFacebook, ExamenesManager.pretestCollection);
     }
     
     @GET
@@ -66,6 +66,6 @@ public class ExamenesRest {
     @Path("realizoExamenPostTest")
     public String realizoExamenPostTest(
             @QueryParam("idFacebook") String idFacebook){
-        return new ExamenesManager().realizoExamenPostTest(idFacebook);
+        return new ExamenesManager().realizoExamen(idFacebook, ExamenesManager.posttestCollection);
     }
 }
